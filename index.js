44210be5310836635cae0d94346858f71903255f
jsgilberto <alv.mtz94@gmail.com>
2018-05-31 19:09:38 +0200

    added navbar functionality

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+var button = document.getElementById("menu");
+var ulright = document.getElementById("sidebar")
+var x = true;
+button.addEventListener("click", function(){
+  
+  if (x) {
+    ulright.style.transform = "translateX(0)";
+    x = false;
+  }
+  else {
+    ulright.style.transform = "translateX(-100vw)";
+    x = true;
+  }
+});
